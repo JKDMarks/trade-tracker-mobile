2e23b8f1d1ab81d9dceaf6ef1ca5de410217e70f
Jeffrey Marks <JKDMarks@gmail.com>
2019-11-19 15:08:27 -0500

    Search through allCards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
-import { Grid } from 'semantic-ui-react'
+import _ from 'lodash'
+import { Grid, Search } from 'semantic-ui-react'
 
 function App() {
+  const [allCards, setAllCards] = useState([])
+  const [searchCards, setSearchCards] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchAllCards() {
+      const resp = await fetch('https://api.scryfall.com/catalog/card-names')
+      const json = await resp.json()
+
+      setAllCards(json.data)
+    }
+
+    fetchAllCards()
+  }, [])
+
+  const handleInputChange = (e, { value }) => {
+    setIsLoading(true)
+    const re = new RegExp(value, 'i')
+    setSearchCards(allCards.filter(card => re.test(card)).slice(0, 5).map(name => ({ name })))
+    setIsLoading(false)
+  }
+
   return (
     <div className='App'>
-      <Grid>
+      <Grid centered>
         <Grid.Row centered className='header'>
-            <h1>Trade Tracker</h1>
+          <h1>Trade Tracker</h1>
         </Grid.Row>
-        <Grid.Row style={{backgroundColor: 'green'}}>
 
+        <Grid.Row style={{backgroundColor: 'green'}}>
+          <Search
+            onSearchChange={_.debounce(handleInputChange, 1000)}
+            results={searchCards}
+            resultRenderer={card => <p>{card.name}</p>}
+            loading={isLoading}
+          />
+        </Grid.Row>
+
+        <Grid.Row className='pt-0' columns={2} style={{height: '25px'}}>
+          <Grid.Column className='p-1' style={{backgroundColor: 'blue', textAlign: 'center'}}>⏪</Grid.Column>
+          <Grid.Column className='p-1' style={{backgroundColor: 'red', textAlign: 'center'}}>⏩</Grid.Column>
         </Grid.Row>
       </Grid>
     </div>
