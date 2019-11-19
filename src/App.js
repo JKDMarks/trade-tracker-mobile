027445eb8a1196c807a5c21fc7fd6c2979d7734a
Jeffrey Marks <JKDMarks@gmail.com>
2019-11-19 13:33:45 -0500

    Semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import { Grid } from 'semantic-ui-react'
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className='App'>
+      <Grid>
+        <Grid.Row centered className='header'>
+            <h1>Trade Tracker</h1>
+        </Grid.Row>
+        <Grid.Row style={{backgroundColor: 'green'}}>
+
+        </Grid.Row>
+      </Grid>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
