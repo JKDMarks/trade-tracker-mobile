1272b023928f95d4229afca76268ffb4804bfe6d
Jeff Marks <JKDMarks@gmail.com>
2025-08-27 13:19:07 -0400

    Update node version

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,5 +1,12 @@
 import React, { Fragment } from "react";
-import { Modal, Grid, Dropdown, Checkbox, Button, Icon } from "semantic-ui-react";
+import {
+    Modal,
+    Grid,
+    Dropdown,
+    Checkbox,
+    Button,
+    Icon,
+} from "semantic-ui-react";
 
 function Overlay({
     card,
@@ -16,7 +23,8 @@ function Overlay({
     setCardImg,
 }) {
     const getCardImageUrl = (cardData) =>
-        cardData?.image_uris?.border_crop || cardData?.card_faces?.[0]?.image_uris?.border_crop;
+        cardData?.image_uris?.border_crop ||
+        cardData?.card_faces?.[0]?.image_uris?.border_crop;
 
     if (card && Object.entries(card).length > 0) {
         const { editions, setIdx, isFoil, quantity } = card;
@@ -31,7 +39,14 @@ function Overlay({
         const cardImageUrl = getCardImageUrl(cardData);
 
         return (
-            <Modal className="ctr-txt" open={isOpen} onClose={closeOverlay} closeIcon closeOnDimmerClick={false}>
+            <Modal
+                open={isOpen}
+                className="ctr-txt"
+                size="fullscreen"
+                onClose={closeOverlay}
+                closeIcon
+                closeOnDimmerClick={false}
+            >
                 <Modal.Content className="vert-ctr-parent">
                     {editions && editions.length > 0 ? (
                         <Grid centered className="vert-ctr">
@@ -47,44 +62,80 @@ function Overlay({
                                     />
                                     <u>
                                         {card.editions[setIdx].name}{" "}
-                                        {editions[0].reserved ? <span style={{ color: "grey" }}>(RL)</span> : null}
+                                        {editions[0].reserved ? (
+                                            <span style={{ color: "grey" }}>
+                                                (RL)
+                                            </span>
+                                        ) : null}
                                     </u>
                                 </Grid.Column>
                             </Grid.Row>
 
                             <Grid.Row className="pt-0">
-                                <Grid.Column textAlign="center">Price Per Card: ${cardPrice(card)}</Grid.Column>
+                                <Grid.Column textAlign="center">
+                                    Price Per Card: ${cardPrice(card)}
+                                </Grid.Column>
                             </Grid.Row>
 
                             <Grid.Row>
                                 <Grid.Column>
-                                    <Dropdown className="selection" fluid text={editions[setIdx].set_name}>
+                                    <Dropdown
+                                        className="selection"
+                                        fluid
+                                        text={editions[setIdx].set_name}
+                                    >
                                         <Dropdown.Menu>
                                             {editions.map((edition, i) => (
                                                 <Dropdown.Item
                                                     key={i}
                                                     value={i}
-                                                    onClick={(e, { value }) => editCardSet(card, value)}
-                                                    selected={editions.setIdx === i}
+                                                    onClick={(e, { value }) =>
+                                                        editCardSet(card, value)
+                                                    }
+                                                    selected={
+                                                        editions.setIdx === i
+                                                    }
                                                     content={
-                                                        <Grid columns={16} style={{ width: "100%" }}>
-                                                            <Grid.Column width={4}>
+                                                        <Grid
+                                                            columns={16}
+                                                            style={{
+                                                                width: "100%",
+                                                            }}
+                                                        >
+                                                            <Grid.Column
+                                                                width={4}
+                                                            >
                                                                 <img
-                                                                    src={getCardImageUrl(edition)}
+                                                                    src={getCardImageUrl(
+                                                                        edition
+                                                                    )}
                                                                     alt="card version preview"
-                                                                    style={{ maxHeight: "60px" }}
+                                                                    style={{
+                                                                        maxHeight:
+                                                                            "60px",
+                                                                    }}
                                                                 />
                                                             </Grid.Column>
-                                                            <Grid.Column width={3}>
+                                                            <Grid.Column
+                                                                width={3}
+                                                            >
                                                                 <div className="vert-ctr-parent">
                                                                     <div className="vert-ctr">
-                                                                        <i className={`ss ss-2x ss-${edition.set}`} />
+                                                                        <i
+                                                                            className={`ss ss-2x ss-${edition.set}`}
+                                                                        />
                                                                     </div>
                                                                 </div>
                                                             </Grid.Column>
-                                                            <Grid.Column width={9}>
+                                                            <Grid.Column
+                                                                width={9}
+                                                            >
                                                                 <div className="vert-ctr-parent">
-                                                                    <div className="vert-ctr">{edition.set_name}</div>
+                                                                    <div className="vert-ctr">
+                                                                        {
+                                                                            edition.set_name
+                                                                        }
+                                                                    </div>
                                                                 </div>
                                                             </Grid.Column>
                                                         </Grid>
@@ -103,8 +154,16 @@ function Overlay({
                                             type="number"
                                             value={quantity}
                                             className="m-0"
-                                            style={{ maxWidth: "25%", height: "25px" }}
-                                            onChange={(e) => editCardQuantity(card, e.target.value)}
+                                            style={{
+                                                maxWidth: "25%",
+                                                height: "25px",
+                                            }}
+                                            onChange={(e) =>
+                                                editCardQuantity(
+                                                    card,
+                                                    e.target.value
+                                                )
+                                            }
                                         />
                                         &nbsp; Quantity
                                     </label>
@@ -116,11 +175,15 @@ function Overlay({
                                             <Checkbox
                                                 label="Foil"
                                                 checked={isFoil}
-                                                onChange={() => toggleFoil(card)}
+                                                onChange={() =>
+                                                    toggleFoil(card)
+                                                }
                                                 disabled={
                                                     isFoil
-                                                        ? !editions[setIdx].prices.usd
-                                                        : !editions[setIdx].prices.usd_foil
+                                                        ? !editions[setIdx]
+                                                              .prices.usd
+                                                        : !editions[setIdx]
+                                                              .prices.usd_foil
                                                 }
                                             />
                                         </div>
@@ -137,7 +200,12 @@ function Overlay({
                                                 className="px-3"
                                                 color="green"
                                                 content="Add Left"
-                                                onClick={() => addToTrade(true) /* ARG OF addToTrade IS isLeft */}
+                                                onClick={
+                                                    () =>
+                                                        addToTrade(
+                                                            true
+                                                        ) /* ARG OF addToTrade IS isLeft */
+                                                }
                                             />
                                         </Grid.Column>
                                         <Grid.Column textAlign="center">
@@ -145,22 +213,34 @@ function Overlay({
                                                 className="px-3"
                                                 color="green"
                                                 content="Add Right"
-                                                onClick={() => addToTrade(false) /* ARG OF addToTrade IS isLeft */}
+                                                onClick={
+                                                    () =>
+                                                        addToTrade(
+                                                            false
+                                                        ) /* ARG OF addToTrade IS isLeft */
+                                                }
                                             />
                                         </Grid.Column>
                                     </Grid.Row>
                                 ) : (
                                     <Fragment>
-                                        <Grid.Row style={{ paddingBottom: "0" }}>
+                                        <Grid.Row
+                                            style={{ paddingBottom: "0" }}
+                                        >
                                             <Grid.Column textAlign="center">
                                                 <Button
                                                     color="red"
                                                     content="Delete From Trade"
                                                     style={{ marginTop: "5px" }}
                                                     onClick={() => {
-                                                        const confirmDelete = window.confirm("Delete this card?");
+                                                        const confirmDelete =
+                                                            window.confirm(
+                                                                "Delete this card?"
+                                                            );
                                                         if (confirmDelete) {
-                                                            deleteFromTrade(card);
+                                                            deleteFromTrade(
+                                                                card
+                                                            );
                                                         }
                                                     }}
                                                 />
@@ -194,17 +274,29 @@ function Overlay({
                             </Grid.Row>
                             <Grid.Row className="pt-0" columns={3}>
                                 <Grid.Column textAlign="center">
-                                    <a target="_blank" rel="noreferrer" href={scryfall_uri}>
+                                    <a
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        href={scryfall_uri}
+                                    >
                                         Scryfall
                                     </a>
                                 </Grid.Column>
                                 <Grid.Column textAlign="center">
-                                    <a target="_blank" rel="noreferrer" href={tcgplayer}>
+                                    <a
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        href={tcgplayer}
+                                    >
                                         TCGplayer
                                     </a>
                                 </Grid.Column>
                                 <Grid.Column textAlign="center">
-                                    <a target="_blank" rel="noreferrer" href={edhrec}>
+                                    <a
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        href={edhrec}
+                                    >
                                         EDHREC
                                     </a>
                                 </Grid.Column>
